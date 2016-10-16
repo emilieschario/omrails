b73cc34d8f717f8f3e35ba225cf9f2e26f5206fe
emilielimaburke <emilie.burke@gmail.com>
2016-10-16 16:50:00 -0400

    move navigation to layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
-  get 'contact-us' => 'pages#contact'
+  get 'contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
