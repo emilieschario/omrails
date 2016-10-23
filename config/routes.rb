2b449bbfc8f05f74029298482a7a9cc3227b7557
emilielimaburke <emilie.burke@gmail.com>
2016-10-23 19:07:25 -0400

    added users & login/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+    get "signin" => 'devise/sessions#new'
+    delete "signout" => 'devise/sessions#destroy'
+    get 'signup' => 'devise/registrations#new'
+  end  
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
